55e7b2ebf5a7e3947b9542f822798d7917474c96
k4nkan <kanta.y.0126@gmail.com>
2025-10-14 00:30:02 +0900

    Initial commit

---
+++ DailyLogs/DailyLogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyLogsApp.swift
+//  DailyLogs
+//
+//  Created by kanta yamauchi on 2025/10/13.
+//
+
+import SwiftUI
+
+@main
+struct DailyLogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
